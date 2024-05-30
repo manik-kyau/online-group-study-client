94c78c968c351bb55d0d6cded5590aa63a4c1c41
manik-kyau <you@example.commaniksarkar.kyau.cse>
2024-05-30 12:13:59 +0600

    Add leaflet map

--- src/Componants/Home/Home.jsx
+++ src/Componants/Home/Home.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import Faq from "../../Pages/Faq/Faq";
 import Banner from "../Banner/Banner";
 import Features from "../Features/Features";
+import Leaflet from "../Leaflet/Leaflet";
 
 const Home = () => {
     return (
@@ -15,6 +16,7 @@ const Home = () => {
                 <Banner></Banner>
                 <Features></Features>
                 <Faq></Faq>
+                <Leaflet></Leaflet>
             </div>
         </div>
     );
